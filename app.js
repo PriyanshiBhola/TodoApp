e6cd353af6ba706578d1417e7f0b86023f267fc4
PriyanshiBhola <43310746+PriyanshiBhola@users.noreply.github.com>
2020-04-15 01:50:23 +0530

    Updated FrontEnd and Updated Functionality

--- app.js
+++ app.js
@@ -42,3 +42,75 @@ app.post('/todos', (req, res) => {
     });
     
 });
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(isNaN(parseInt(req.params.id)))
+    {
+        req.status(404).send(
+            {
+                error : 'invalid todo id'
+            });
+            return;
+    }
+    Todos.findOne({
+        attributes : ['id', 'title', 'description', 'due_date', 'status', 'priority'],
+        where : 
+        {
+            'id' : id
+        }
+    }).then((data) => {
+        res.send(data);
+    }
+    );
+});
+
+app.post('/todos/:id/notes', (req, res) => {
+    id = req.params.id
+    data = req.body
+    data.TodoId = Number(id)
+    Todos.findAll({
+        attributes : ['id']
+    })
+    .then((list) => {
+        for(let item of list){
+            if(item.id == id)
+            {notes.create(data).then(() => res.send(""))
+            return
+
+            }
+        }
+        res.status(404)
+        res.send({error : 'Wrong Id'})
+    })
+})
+
+app.get('/todos/:id/notes', (req, res) => 
+{
+    id = req.params.id
+    notes.findAll({
+        attributes : ['note'],
+        where :
+        {
+            TodoId : id
+        }
+
+    }).then((data) => res.send(data))
+})
+
+app.patch('/todos/:id', (req, res) =>
+{
+    let Taskid = req.params.id
+    let data = req.body
+    Todos.findOne({
+        where : {
+            id : Taskid
+        }
+    })
+    .then((todo) => {
+        todo.due = data.due
+        todo.priority = data.priority
+        todo.status = data.status
+        todo.save().then(()=> res.send(""))
+    })
+})
+
